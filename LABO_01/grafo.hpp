6827227c59c2996d235d815910c7c934babfdebc
Abel Hancco <abel.hancco@ucsp.edu.pe>
2020-05-07 16:21:00 -0500

    v

--- LABO_01/grafo.hpp
+++ LABO_01/grafo.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <stack>
-#include <utility>  
+#include <utility>
 #include <windows.h>
 #include <iostream>
 #include <math.h>
@@ -9,273 +9,248 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <list>
 #include <GL/glut.h>
-
 
 using namespace std;
 
-class punto {
-	public:
-		int x;
-		int y;
-		bool visitado;
-		bool agregado;
-		float costo;
-		punto() { x = y = 0; visitado = agregado = false; }
+#define infinito 10000.0f
 
-
-		punto operator=(const punto &A) {
-			x = A.x;
-			y = A.y;
-			return *this;
-		}
-
-		bool operator== (const punto &B) {
-			if (this->x == B.x && this->y == B.y) {
-				return true;
-			}
-			return false;
-		}
-
-};
 
 class nodo {
 public:
-	int dato;
-	int x;  //para openGL
-	int y;  //para openGL
-	nodo** child;
-	nodo* tmp;
+    int dato;
+    int x;  ///para openGL
+    int y;  ///para openGL
 
-	nodo() {//para hijos
-		dato = x = y = 66;
-		child = new  nodo * [2];
-		child[0] = nullptr;
-		child[1] = nullptr;
-	}
+    bool bloqueado;
+    bool visitado;	///para A*
+    float local;
+    float global;
+    vector<nodo*> vecinos;
+    nodo* parent;	///reconstruir camino
+    nodo* nodes;    ///contendra la matrix
+    list<nodo*> nodos; ///ordenados
+    vector<nodo*> nodo_cuadricula; ///ordenados
 
-	nodo(int cantidad, int validar) {//para hijos
-		dato = x = y = 99;
-		child = new  nodo * [cantidad];
-	}
+    nodo() {//para hijos
+        dato = x = y = -1;
+        bloqueado = false;
+        visitado = false;	///para A*
+        local = infinito;
+        global = infinito;
+        nodes = nullptr;
+        parent = nullptr;	/// reconstruir camino
+    }
 
-	nodo(int cantidad) {//para root
-		dato = x = y = 1;
-		child = new nodo * [cantidad];
-		for (std::size_t i = 0; i < cantidad; ++i) {///inicializamos 100 primeros nodos con valor i
-			child[i] = new nodo();
-			child[i]->setDATA(i);
-			child[i]->setXY(0, i);
-			if (i > 0) {
-				child[i - 1]->child[0] = child[i];
-			}
-		}
-		child[0]->child[1] = new nodo();
-		child[0]->child[1]->setDATA(100);
-		child[0]->child[1]->setXY(1, 0);
-		child[97]->child[1] = new nodo();
-		child[97]->child[1]->setDATA(197);
-		child[97]->child[1]->setXY(1, 97);
-		child[98]->child[1] = new nodo();
-		child[98]->child[1]->setDATA(198);
-		child[98]->child[1]->setXY(1, 98);
-		child[99]->child[1] = new nodo();
-		child[99]->child[1]->setDATA(199);
-		child[99]->child[1]->setXY(1, 99);
+    nodo(int cantidad) {//para root
+        dato = x = y = -1;
+        bloqueado = false;
+        visitado = false;	///para A*
+        local = infinito;
+        global = infinito;
+        nodes = nullptr;
+        parent = nullptr;	/// reconstruir camino
 
-		child[0]->child[1]->child[1] = new nodo();
-		child[0]->child[1]->child[1]->setDATA(200);
-		child[0]->child[1]->child[1]->setXY(2, 0);
-		child[97]->child[1]->child[1] = new nodo();
-		child[97]->child[1]->child[1]->setDATA(297);
-		child[97]->child[1]->child[1]->setXY(2, 97);
-		child[98]->child[1]->child[1] = new nodo();
-		child[98]->child[1]->child[1]->setDATA(298);
-		child[98]->child[1]->child[1]->setXY(2, 98);
-		child[99]->child[1]->child[1] = new nodo();
-		child[99]->child[1]->child[1]->setDATA(299);
-		child[99]->child[1]->child[1]->setXY(2, 99);
+    }
 
-		tmp = new nodo(100, 1);
-		child[0]->child[1]->child[1]->child[1] = new nodo();
-		child[0]->child[1]->child[1]->child[1]->setDATA(300);
-		child[0]->child[1]->child[1]->child[1]->setXY(3, 0);
-
-		//child[0]->child[1]->child[1]->child[1]->child[0] = new nodo();
-		//child[0]->child[1]->child[1]->child[1]->child[0]->setDATA(400);
-		//child[0]->child[1]->child[1]->child[1]->child[0]->setXY(4,0);
-
-		//tmp->child[0] = child[0]->child[1]->child[1]->child[1]->child[0];
-		//nodo *lateral = child[0]->child[1]->child[1]->child[1]->child[0];
-
-		tmp->child[0] = child[0]->child[1]->child[1]->child[1];
-		nodo* lateral = child[0]->child[1]->child[1]->child[1];
-		nodo* copia = lateral;
-		//lateral = new nodo();
-		//lateral->setDATA(300+1);
-		//lateral->setXY(3,1);
+    void inicializacion(int x__, int y__) {
+        nodes = new nodo[x__ * y__];
+        nodo_cuadricula.reserve(10000);
+        int valor_ = 0;
+        for (int x = 0; x < x__; x++)
+            for (int y = 0; y < x__; y++)
+            {
+                nodes[y * x__ + x].dato = valor_;
+                nodes[y * x__ + x].x = x;
+                nodes[y * x__ + x].y = y;
+                nodes[y * x__ + x].bloqueado = false;
+                nodes[y * x__ + x].parent = nullptr;
+                nodes[y * x__ + x].visitado = false;
+                nodes[y * x__ + x].local = infinito;
+                nodes[y * x__ + x].global = infinito;
+                valor_++;
+            }
 
 
-		for (std::size_t i = 1; i < cantidad; ++i) {
-			//child[0]->child[1]->child[1]->child[1]->child[0] = new nodo();
-			//nodo *lateral = child[0]->child[1]->child[1]->child[1]->child[0];
-			lateral = new nodo();
-			lateral->setDATA(300 + i);
-			lateral->setXY(3, i);
+        for (int x = 0; x < x__; x++)
+            for (int y = 0; y < y__; y++)
+            {
+                if (y > 0)
+                    nodes[y * x__ + x].vecinos.push_back(&nodes[(y - 1) * x__ + (x + 0)]);
+                if (y < x__ - 1)
+                    nodes[y * x__ + x].vecinos.push_back(&nodes[(y + 1) * x__ + (x + 0)]);
+                if (x > 0)
+                    nodes[y * x__ + x].vecinos.push_back(&nodes[(y + 0) * x__ + (x - 1)]);
+                if (x < x__ - 1)
+                    nodes[y * x__ + x].vecinos.push_back(&nodes[(y + 0) * x__ + (x + 1)]);
 
+            }
 
-			copia->child[0] = lateral;
-			copia = lateral;
+        for (int i = 0; i < x__ * x__; ++i) {
+            nodos.push_back(&nodes[i]);
+        }
+        nodos.sort([](const nodo* lhs, const nodo* rhs) {return lhs->dato < rhs->dato; });
+        while (!nodos.empty()) {
+            nodo_cuadricula.push_back(nodos.front());
+            nodos.pop_front();
+        }
 
+    }
 
-			//tmp->child[i-1] = lateral;
-			///tmp->child[0] = lateral;
-			tmp->child[i] = lateral;
-			lateral = tmp->child[i];
-		}
+    void bloquear(int cantidad) {
+        //for (auto i : nodo_cuadricula) {
+        for (std::size_t i = 0; i < cantidad; ++i) {
+            int rand_ = (rand() % (99 * 99)) + 1;
+            //cout << (rand() % (99*99) ) + 1 << endl;
+            nodo_cuadricula[rand_]->bloqueado = true;
+        }
+        for (std::size_t i = 0; i < 40; ++i) {
+            nodo_cuadricula[5520 + i]->bloqueado = true;
+        }
+    }
 
-		//child[0]->child[1]->child[1]->child[1]
-		child[97]->child[1]->child[1]->child[1] = tmp->child[97];
+    void setXY(int x, int y) {
+        this->x = x;
+        this->y = y;
+    }
 
-		child[98]->child[1]->child[1]->child[1] = tmp->child[98];
-
-		child[99]->child[1]->child[1]->child[1] = tmp->child[99];
-
-	}
-
-	//nodo* tmp = tmp2->add100(A.root, 100, 100, 1);
-	nodo* add100(nodo*& root, std::size_t cant, unsigned int valor, int x) {
-		nodo* return_;
-		return_ = new nodo(cant, 1);
-
-		for (std::size_t i = 0; i < cant; ++i) {///inicializamos 100 nodos con valor
-			root->child[i]->child[1] = new nodo();
-			root->child[i]->child[1]->setDATA(i + valor);
-			root->child[i]->child[1]->setXY(x, i);
-
-			if (i > 0) {
-				root->child[i - 1]->child[1]->child[0] = root->child[i]->child[1];
-			}
-			return_->child[i] = root->child[i]->child[1];
-			//cout << "FOR " << return_->child[0]->x << " " << return_->child[0]->y << endl;
-		}
-		return return_;
-	}
-
-	/**
-	void eliminar(nodo* current, nodo*& root) {///ANCHURA/AMPLITUD
-		///cout << "elimnar " <<current->dato <<endl;
-		//cout << root->child[1]->dato;
-		for (std::size_t i = 2; i < 20; ++i) {
-			root->child[i]->child[1] = nullptr;
-		}
-		root->child[1]->child[0] = nullptr;
-	
-	}*/
-
-	void setXY(int x, int y) {
-		this->x = x;
-		this->y = y;
-	}
-	void setDATA(int data) {
-		this->dato = data;
-	}
+    void setDATA(int data) {
+        this->dato = data;
+    }
 };
 
 
 class grafoIA {
 public:
-	nodo* root;
-	int camino[200];
-	int caminoX[200];
-	int caminoY[200];
-	int tamanho;
+    nodo* root;
+    int tamanho;
+    int cantidad;
+    vector<nodo*> solucionA;
+    vector<nodo*> visitados;
+    vector<nodo*> solucionAnchura;
 
-	struct struc_comparar {
-		bool operator()(punto *&P, punto *&Q) {
-			return P->costo > Q->costo;
-		}
-	};
+    //vector<nodo*> copia;
 
-	priority_queue<punto *, vector<punto* >, struc_comparar > menor;
-
-	vector<punto *> Path;
-	
-
-		//int puntoB[144];
-	grafoIA(int cantidad) {
-		root = new nodo(cantidad);
-		root->setDATA(-1);
-		tamanho = 0;
-
-	}
+    grafoIA(int cantidad) {
+        root = new nodo(cantidad);
+        root->setDATA(-1);
+        tamanho = 0;
+        this->cantidad = cantidad;
+        root->inicializacion(cantidad, cantidad);
+        ///solucionAnchura.reserve(10000);
+    }
 
 
-	void ANCHURA(nodo* destino,  nodo*& root) {///ANCHURA/AMPLITUD
-		queue<nodo*> cola;
-		
-		int indice = 0;
-		for (std::size_t i = 0; i < 100; ++i) {
-			cola.push(root->child[i]);
-		}
-		while (!cola.empty()) {
-			if (cola.front()) {
-				cout << cola.front()->dato << " ";
-				caminoX[indice] = cola.front()->x;
-				caminoY[indice] = cola.front()->y;
-				indice++;
-		
-				cola.push(cola.front()->child[1]);
-			}
-			cola.pop();
-		}
-	}
+    void ANCHURA(nodo* inicio, nodo* destino) {///ANCHURA/AMPLITUD
+        queue<nodo*> cola;
+        vector<nodo*> copia(root->nodo_cuadricula);
+        int i = 0;
+        while (inicio->dato != root->nodo_cuadricula[i]->dato) {
+            copia.erase(copia.begin());
+            i++;
+        }
+        while (destino->dato != root->nodo_cuadricula[i]->dato) {
+            i++;
+        }
+
+        int cc = (inicio->dato) % cantidad;
+        int i_ = (inicio->dato) / cantidad;
+        int k = 0;
+
+        for (int ii = i_; ii < cantidad; ++ii) {
+            for (int j = cc; j < cantidad; ++j) {
+                if (copia[k]->bloqueado == false && destino->dato > copia[k]->dato && copia[k]->bloqueado == false) {
+                    cola.push(copia[k]);
+                    //solucionAnchura.push_back(copia[k]);
+                }
+                i_++;
+                k++;
+            }
+            //i = 0;
+        }
+
+        while (root->nodo_cuadricula[i] != nullptr) {
+            //if (root->nodo_cuadricula[i]->bloqueado == false) {
+            copia.pop_back();
+            i++;
+            //} 
+        }
+
+        copia.swap(solucionAnchura);
+        /**
+        for (std::size_t indice = 0; indice < solucionAnchura.size(); ++indice) {
+            if (copia[i]->bloqueado == false) {
+                solucionAnchura.push_back(copia[i]);
+            }
+        }
+        ****/
+
+    }
 
 
+    ///float euclides(nodo *A, nodo *B) {
+    float euclides(nodo* A, nodo* B)
+    {
+        return static_cast<float>(sqrt(pow(abs(B->x - A->x), 2) + pow(abs(B->y - A->y), 2)));
+    }
 
-	float euclides(punto P, punto N) {
-		return sqrt(pow(abs(N.x - P.x), 2) + pow(abs(N.y - P.y), 2));
-	}
+    float manhattan(nodo* A, nodo* B)
+        ///float euclides(nodo* A, nodo* B)
+    {
+        return static_cast<float>((abs(B->x - A->x) + abs(B->y - A->y)));
+    }
 
-	///euclidean
+    float octagonal(nodo* A, nodo* B)
+        //float euclides(nodo *A, nodo *B)
+    {
+        return static_cast<float>((abs(B->x - A->x) + abs(B->y - A->y)) + (-6) * min(abs(B->x - A->x), abs(B->y - A->y)));
+    }
 
-	bool A_asterisco(punto &P, punto &N, nodo *root) {
-		float G;
-		float H;
-		int indice = 0;
-		punto *visitado;
-		menor.push(&P);
-		while (!menor.empty()) {
-			camino[indice] = visitado->x;
-			camino[indice] = visitado->y;
-			indice++;
-			visitado = menor.top();
-			visitado->visitado = true;
-			menor = priority_queue<punto*, vector<punto* >, struc_comparar>();
-			if (*visitado == N) {
-				return true;
-			}
-			
-			else {
-				for (unsigned int i = 0; i < 100; i++) {
-					if (root->child[i]->child[1] == false) {
-						G = euclides(P, N);
-						punto A;
-						A.x = root->child[i]->child[1]->x;
-						A.x = root->child[i]->child[1]->y;
-						H = euclides(A, N);
-						root->child[i]->child[1] = root->child[i]->child[1]->child[1];
-						visitado->visitado = G + H;
-						menor.push(visitado);
-					}
-				}
-			}
+    bool A_asterico(nodo* inicio, nodo* final_) {
+        nodo* actual = inicio;	///explorando actual
+        inicio->local = 0.0f;
+        inicio->global = euclides(inicio, final_);
+        list<nodo*> lista_noProbados; ///para probar todos posibles nodo hasta final
+        lista_noProbados.push_back(inicio);
 
-			
-		}
-		return false;
-	}
+        while (!lista_noProbados.empty() && actual != final_) {
+            lista_noProbados.sort([](const nodo* lhs, const nodo* rhs) {return lhs->global < rhs->global; });
+            while (!lista_noProbados.empty() && lista_noProbados.front()->visitado) {
+                lista_noProbados.pop_front();
+            }
+            if (lista_noProbados.empty())
+                break;
 
+            actual = lista_noProbados.front();
+            actual->visitado = true;
+            for (auto nodoVecino : actual->vecinos) {   ///revisar vecinos
+                //if (!nodoVecino->visitado && nodoVecino != nullptr) {//si ya visitado o es obstaculo
+                if (!nodoVecino->visitado && nodoVecino->bloqueado == false) {//si ya visitado o es obstaculo
+                    lista_noProbados.push_back(nodoVecino);
+                    visitados.push_back(nodoVecino);///BORRAR
+                }
+                float PosibleMenor = actual->local + euclides(actual, nodoVecino);
+                if (PosibleMenor < nodoVecino->local) {
+                    nodoVecino->parent = actual;
+                    nodoVecino->local = PosibleMenor;
 
+                    nodoVecino->global = nodoVecino->local + euclides(nodoVecino, final_);
+                }
+            }
+        }
+        /**
+        for(auto i : lista_noProbados )
+            visitados.push_back(i);
+        cout << "tamanho " << visitados.size() << endl;
+        */
+        nodo* temp = final_->parent;
+        solucionA.push_back(final_);
+        while (temp->parent != nullptr) {
+            solucionA.push_back(temp);
+            temp = temp->parent;
+        }
+        solucionA.push_back(inicio);
+        return true;
+    }
 };
-
